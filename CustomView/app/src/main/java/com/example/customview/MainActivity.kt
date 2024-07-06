d0cd311bf7666d1b186ea15450fc4d58d0e57a68
Olga <oola2611@gmail.com>
2024-07-06 23:08:25 +0300

    custom view

---
+++ CustomView/app/src/main/java/com/example/customview/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.customview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.customview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.upperString.text = "Верхняя строка"
+        binding.lowerString.text = "Нижняя строка"
+    }
+}
